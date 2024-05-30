f00ed77e6b3927a18b3b52cc3e64cece9e94a9a0
Pere Antoni <56586435+sys-walker@users.noreply.github.com>
2024-05-30 09:50:01 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,38 @@
+console.log("WTF IS NOT TO PAY TO REJECT COOKIEEEEEEES!!!!");
+
+async function main() {
+  const cookiesModal = await new Promise((resolve) => {
+    setTimeout(() => {
+      let d = document.getElementById("didomi-host");
+
+      resolve(d);
+    }, 1500);
+  });
+  if (!cookiesModal) {
+    console.log("Didomi Host not found");
+    return;
+  } else {
+    console.log("Didomi Host found: ", cookiesModal);
+    cookiesModal.remove();
+  }
+
+  let body = document.getElementsByTagName("body");
+  let bodyDoc = body[0];
+  let bodyClassList = bodyDoc.classList;
+
+  let classNameToRemove = "";
+  for (let i = 0; i < bodyClassList.length; i++) {
+    if (bodyClassList[i].includes("didomi")) {
+      classNameToRemove = bodyClassList[i];
+      break;
+    }
+  }
+  if (classNameToRemove !== "") {
+    bodyDoc.classList.remove(classNameToRemove);
+    log("Class removed: ", classNameToRemove);
+  } else {
+    console.log("No class found to remove");
+  }
+}
+
+main();
